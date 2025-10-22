4e99f238c7d84c779272bf20beaf5a9b251004b8
Angelo Hoeung <hi@hoeung.ca>
2025-10-22 14:11:01 -0400

    update manual stop picker logic to be clickable

--- src/components/map/StopMapPicker.jsx
+++ src/components/map/StopMapPicker.jsx
@@ -2,43 +2,47 @@ import { useCallback, useState, useEffect, useRef } from "react";
 
 import { useParams, useLocation } from "wouter";
 
-import { useMap, useMapEvents } from "react-leaflet";
+import { useMapEvent } from "react-leaflet";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleDot, faCheck } from "@fortawesome/free-solid-svg-icons";
+import { faCircleDot, faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 import { useUpdateStop } from "../../atoms/stops";
 
 import CustomControls from './controls/CustomControls';
 
 export default function StopMapPicker() {
-  const [center, setCenter] = useState({ lat: null, lng: null });
-  const map = useMap();
-  useEffect(
-    () => {
-      setCenter(map.getCenter());
-    },
-    []
-  );
-  useMapEvents({
-    moveend() {
-      setCenter(map.getCenter());
-    },
-  });
+  const [selected, setSelected] = useState(null); // { lat, lng } or null
 
   const updateStop = useUpdateStop();
 
   const params = useParams();
   const [, navigate] = useLocation();
+
+  // check for clicks on the map to set the marker
+  useMapEvent('click', (e) => {
+    // only the clicks from the map container are registered to lat/lng
+    if (e.originalEvent.target.closest('.leaflet-container')) {
+      setSelected(e.latlng);
+    }
+  });
+
   const handleAccept = useCallback(
     () => {
-      if ((!("stopIndex" in params) || params.stopIndex == null)) {
+      if (!selected || !("stopIndex" in params) || params.stopIndex == null) {
         return;
       }
-      updateStop({ index: new Number(params.stopIndex), ...center });
+      updateStop({ index: new Number(params.stopIndex), ...selected });
       navigate("/stops");
     },
-    [center, params, updateStop, navigate],
+    [selected, params, updateStop, navigate],
+  );
+
+  const handleCancel = useCallback(
+    () => {
+      navigate("/stops");
+    },
+    [navigate],
   );
 
   const [position, setPosition] = useState({ top: 0, left: 0 });
@@ -51,30 +55,38 @@ export default function StopMapPicker() {
         left: (rect.height / 2) * -1,
       });
 
-      const observer = new MutationObserver(() => {
-        const rect = elementRef.current.getBoundingClientRect();
-        setPosition({
-          top: (rect.height / 2) * -1,
-          left: (rect.height / 2) * -1,
-        });
+    const observer = new MutationObserver(() => {
+      const rect = elementRef.current.getBoundingClientRect();
+      setPosition({
+        top: (rect.height / 2) * -1,
+        left: (rect.height / 2) * -1,
       });
+    });
 
-      observer.observe(elementRef.current, {
-        attributes: true,
-        subtree: true,
-      });
-
-      return () => {
-        observer.disconnect();
-      };
-    },
-    [],
-  );
+    observer.observe(elementRef.current, {
+      attributes: true,
+      subtree: true,
+    });
+    
+    return () => {
+      observer.disconnect();
+    };
+  },
+  [],
+);
 
   return (
     <>
-      <button type="button" onClick={handleAccept}>
+      <button
+        type="button"
+        onClick={handleAccept}
+        disabled={!selected} // disabled and greyed out until user clicks on map
+        style={{ opacity: !selected ? 0.5 : 1 }}
+      >
         <FontAwesomeIcon icon={faCheck} />
+      </button>
+      <button type="button" onClick={handleCancel}>
+        <FontAwesomeIcon icon={faXmark} />
       </button>
 
       <CustomControls useLeafletStyles={false} position="center">
